cbc38c5de251f74a4facfd4b09d1c63eddf9bca3
sumithkk <sumithkk2007@gmail.com>
2023-12-09 00:02:35 +0530

    Changes in homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,14 @@ import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
 import '../styles/global.css';
+import Head from 'next/head';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Home</title>
+      </Head>
       <Component {...pageProps} />
     </Provider>
   );
